7c37557c87e4709a0fea1babf9aa8af47876d061
paras8 <paras.solanki@capgemini.com>
2023-09-04 20:45:15 +0530

    adds security

--- src/main/java/com/apigateway/controller/TestController2.java
+++ src/main/java/com/apigateway/controller/TestController2.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api")
 public class TestController2 {
@@ -12,9 +14,10 @@ public class TestController2 {
 	
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/get/Test2")
-	public String getData()
-	{
-		return "Test  Data From Service 2 ADMIN";
+	public String getData(HttpServletRequest request)
+	{   String header = request.getHeader("Authorization");
+	System.out.println(header);
+		return header+"  Test  Data From Service 2 ADMIN";
 	}
 	@PreAuthorize("hasRole('USER')")
 	@GetMapping("/getUser/Test2")
