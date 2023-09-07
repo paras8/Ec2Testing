25b8e3eb751939faf4a69c53f8585c392db3c9a0
paras8 <paras.solanki@capgemini.com>
2023-09-08 02:34:08 +0530

    adds security parts

--- src/main/java/com/apigateway/controller/TestController.java
+++ src/main/java/com/apigateway/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.apigateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,16 +18,25 @@ public class TestController {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/get/Test3")
-	public String getData()
-	{
+	public String getData(@RequestHeader("Authorization") String header) {
 		String response = "Test  Data From Service 1";
-		String secondService = "http://13.127.64.164:8082/api/get/Test2"; 
-    
-	     response =  restTemplate.getForObject(secondService,String.class);
-	
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", header);
+
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+
+		String secondService = "http://43.204.108.186:8082/api/get/Test2";
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(secondService, HttpMethod.GET, entity,
+				String.class);
+
+		response = responseEntity.getBody();
+
 		return response;
-		
+
 	}
 }
